1dbf52509fd63ea98b569cdbb64e84410eedafd3
Sarah Peterson <sarahmasonpeterson@gmail.com>
2017-05-19 22:09:28 -0400

    added more months

--- Birthdays.rb
+++ Birthdays.rb
@@ -2,8 +2,12 @@ puts "What month were you born in?"
 month = gets.chomp.downcase
 
 case month
-	when "january", "febaruary", "march"
+	when "january", "february", "december"
 		puts "You were born in the Winter!"
-	when "april", "may", "june"
+	when "april", "may", "march"
 		puts "You were born in the Spring!"
+	when "june", "july", "august"
+		puts "You were born in the Summer!"
+	when "september", "october", "november"
+		puts "You were born in the Fall!"
 	end
