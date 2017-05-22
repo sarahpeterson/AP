84773d9a5ea29956fc1e3dd6bfa8757dbef6c70b
Sarah Peterson <sarahmasonpeterson@gmail.com>
2017-05-22 14:40:58 -0400

    added new GardenBox project that works

---
+++ EmptyClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+namespace Gardenbox
+{
+	public static class tClass
+	//MainClass useClass = new MainClass();
+
+	{
+
+		public static int SquareFeet(int width, int length)
+		{
+			//multiply length and width for square footage of their garden
+			int square = (width * length);
+			return square;
+		}
+
+		public static void printPlants()
+		{
+
+			SqliteConnection connection = new SqliteConnection(@"Data Source=/Users/sarahpeterson/Desktop/GitLab/GardenBox/gardenbox.sqlite");
+			connection.Open();
+
+			SqliteCommand command;
+			SqliteDataReader reader;
+
+			//display all plant options, ask which plants they would like to plant
+			command = new SqliteCommand("SELECT * FROM Plants;", connection);
+			reader = command.ExecuteReader();
+
+			if (reader.HasRows)
+			{
+				while (reader.Read())
+				{
+					Console.WriteLine($"{reader["Plant"]}");
+				}
+			}
+
+			reader.Close();
+		}
+
+		public static int PlantFeet(string user_plant)
+		{
+			SqliteConnection connection = new SqliteConnection(@"Data Source=/Users/sarahpeterson/Desktop/GitLab/GardenBox/gardenbox.sqlite");
+			connection.Open();
+
+			SqliteCommand command;
+			SqliteDataReader reader;
+			//select square feet of the user's plant choice from the database
+			command = new SqliteCommand($"SELECT SqFeet FROM Plants WHERE Plant = '{user_plant}'", connection);
+			reader = command.ExecuteReader();
+			int feet = Convert.ToInt32(reader["SqFeet"]);
+
+			reader.Close();
+			connection.Close();
+			return feet;
+
+		}
+
+
+		public static string SqFootage(int squareFeet, int printPlant)
+		{
+
+			//divide the garden's square feet by the plant's square feet
+
+			int number = (squareFeet / printPlant);
+			string footage = number.ToString();
+
+			return footage;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using Gardenbox;
+
+
+namespace GardenBox
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			bool keepPlanting = true;
+
+			//ask for the width of their garden
+			Console.WriteLine("What is the width of your garden?");
+			int width = Convert.ToInt32(Console.ReadLine());
+
+			//ask for the length of their garden
+			Console.WriteLine("What is the length of your garden?");
+			int length = Convert.ToInt32(Console.ReadLine());
+
+			int squareFeet = tClass.SquareFeet(width, length);
+
+			while (keepPlanting == true)
+			{
+
+					Console.WriteLine("Would you like to plant some items in your garden? Yes or No");
+					string input = Console.ReadLine().ToLower();
+
+					if (input == "yes")
+					{
+					tClass.printPlants();
+
+					Console.WriteLine("Which plant would you like to plant?");
+					string user_plant = Console.ReadLine();
+					//split into two functions
+
+					int printPlant = tClass.PlantFeet(user_plant);
+
+					string squarefootage = tClass.SqFootage(squareFeet, printPlant);
+
+					//tell the user how many of their plants they can plant
+					Console.WriteLine($"You can plant {squarefootage} {user_plant}(s) in your garden!");
+
+				}
+
+				else if (input == "no")
+				{
+					Console.WriteLine("Okayyy, Byeee");
+					keepPlanting = false;
+				}
+				else
+				{
+					Console.WriteLine("Please say Yes or No!");
+				}
+
+			}
+		}
+	}
+}
